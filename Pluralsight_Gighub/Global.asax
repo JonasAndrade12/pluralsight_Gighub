104f09f6a97d7371b98a9a6f0b78283a0150030a
Jonas Andrade <jonas.andrade@farfetch.com>
2019-05-03 15:12:56 +0100

    Initial commit

---
+++ Pluralsight_Gighub/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Pluralsight_Gighub.MvcApplication" Language="C#" %>
---
+++ Pluralsight_Gighub/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Pluralsight_Gighub.Startup))]
+namespace Pluralsight_Gighub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
