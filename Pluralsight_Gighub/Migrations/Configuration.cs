89f18704714b9429b67ed05f756bf20f01002ab5
Jonas Andrade <jonas.andrade@farfetch.com>
2019-05-03 18:04:31 +0100

    Models - 5.3

---
+++ Pluralsight_Gighub/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace Pluralsight_Gighub.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Pluralsight_Gighub.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Pluralsight_Gighub.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ Pluralsight_Gighub/Models/Gig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Pluralsight_Gighub.Models
+{
+    public partial class Gig
+    {
+        public int MyProperIDty { get; set; }
+        public ApplicationUser Artist { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public string Venue { get; set; }
+    }
+}
