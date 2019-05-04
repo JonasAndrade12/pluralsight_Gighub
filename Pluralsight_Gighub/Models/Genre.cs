d7bb2540b287ff70fd197b4427948b7417a833e6
Jonas Andrade <jonas_andrade@live.com.pt>
2019-05-04 14:30:38 +0100

    feat: Populate Genre Table

--- Pluralsight_Gighub/Models/Genre.cs
+++ Pluralsight_Gighub/Models/Genre.cs
@@ -1,8 +1,13 @@
-﻿namespace Pluralsight_Gighub.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Pluralsight_Gighub.Models
 {
     public class Genre
     {
         public byte Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
     }
 }
