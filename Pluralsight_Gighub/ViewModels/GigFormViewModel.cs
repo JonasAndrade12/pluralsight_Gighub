038ccb4f9af7b0aac4515d7b4c677e28cd16dae3
Jonas Andrade <jonas.andrade@farfetch.com>
2019-07-05 12:24:14 +0100

    feat: implement saving gig

--- Pluralsight_Gighub/ViewModels/GigFormViewModel.cs
+++ Pluralsight_Gighub/ViewModels/GigFormViewModel.cs
@@ -11,7 +11,7 @@ namespace Pluralsight_Gighub.ViewModels
 
         public string Time { get; set; }
 
-        public int Genre { get; set; }
+        public byte Genre { get; set; }
 
         public IEnumerable<Genre> Genres { get; set; }
     }
