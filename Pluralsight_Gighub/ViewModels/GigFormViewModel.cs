b258ce99eadca27fcb40242dfb5d29ef1a3ba99f
Jonas Andrade <jonas_andrade@live.com.pt>
2019-07-27 12:03:15 +0100

    feat: client side validation

--- Pluralsight_Gighub/ViewModels/GigFormViewModel.cs
+++ Pluralsight_Gighub/ViewModels/GigFormViewModel.cs
@@ -1,21 +1,30 @@
 ﻿using Pluralsight_Gighub.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Pluralsight_Gighub.ViewModels
 {
     public class GigFormViewModel
     {
+        [Required]
         public string Venue { get; set; }
 
+        [Required]
+        [FutureDate]
         public string Date { get; set; }
 
+        [Required]
+        [ValidTime]
         public string Time { get; set; }
 
+        [Required]
         public byte Genre { get; set; }
 
+        [Required]
         public IEnumerable<Genre> Genres { get; set; }
 
+        [Required]
         public DateTime DateTime
         {
             get
